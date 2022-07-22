909c47d6d833e5de03e77ffb8c2f7ce1a318ac29
dk <zjy18830760471@163.com>
2022-07-22 14:05:07 +0800

    second commit

--- src/main/java/com/Coinisi/git/Test.java
+++ src/main/java/com/Coinisi/git/Test.java
@@ -3,5 +3,6 @@ package com.Coinisi.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("name local");
     }
 }
