a4f4f466aad8fb300f2c367391c0a2f883015396
Coisinikk <109730050+Coisinikk@users.noreply.github.com>
2022-07-22 15:09:28 +0800

    poll commit

--- src/main/java/com/Coinisi/git/Test.java
+++ src/main/java/com/Coinisi/git/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("Coisinikk");
+        System.out.println("github is no");
     }
 }
