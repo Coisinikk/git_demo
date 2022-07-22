e731e36bf1be8984cb355edfea8831c5f23e1210
dk <zjy18830760471@163.com>
2022-07-22 14:40:09 +0800

    push commit

--- src/main/java/com/Coinisi/git/Test.java
+++ src/main/java/com/Coinisi/git/Test.java
@@ -4,9 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("name local");
-        System.out.println("Coisini kk");
         System.out.println("00000");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("Coisinikk");
     }
 }
